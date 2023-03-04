8d14123a9b0b4535b0db7405e7b93c54e84287cd
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-03-04 16:05:17 +0100

    Added makefile

---
+++ lab-1/zad1/zad1.h
@@ -0,0 +1,11 @@
+typedef struct Table Table;
+
+Table tab_init(int size);
+
+void count_file(const char * filename, Table * tab);
+
+char * block_content(int index, Table * tab);
+
+void remove_block(int index, Table * tab);
+
+void free_table(Table * tab);
