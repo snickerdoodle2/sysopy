bb9c0d0acfcb47e956b7d56c2e8a4abb5dafb720
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-03-22 14:20:28 +0100

    lab-4 zad-1

---
+++ lab-4/zad-1/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+
+void handle_signal(int sig_code) {
+	printf("Otrzymano sygnał: %d\n", sig_code);
+}
+
+int main(int argc, char ** argv) {
+	if (argc < 2) {
+		printf("Więcej argumentów!\n");
+		return 1;
+	}
+
+	char * command = argv[1];
+
+	sigset_t newmask, oldmask, set;
+
+	if (strcmp(command, "ignore") == 0){
+		signal(SIGUSR1, SIG_IGN);
+	} else if (strcmp(command, "handler") == 0){
+		signal(SIGUSR1, handle_signal);
+	} else if (strcmp(command, "mask") == 0 || strcmp(command, "pending") == 0){
+		sigemptyset(&newmask);
+		sigaddset(&newmask, SIGUSR1);
+		int res = sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+		if (res < 0) {
+			printf("Nie udało się zablokować sygnału\n");
+			return 1;
+		}
+
+	} else {
+		printf("Nie znaleliozno komendy!\n");
+		return 1;
+	}
+
+	raise(SIGUSR1);
+	int child = fork();
+	if (child == 0) {
+		if (strcmp(command, "pending") == 0){
+			sigpending(&set);
+			if (sigismember(&set, SIGUSR1) == 1) {
+				printf("SIGUSR1 czeka w masce (dziecko)!\n");
+			}
+		} else raise(SIGUSR1);
+	}
+
+	return 0;
+}
