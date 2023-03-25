bb7d7f936cf91edebef58ab5367c85f4c808290e
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-03-25 17:56:24 +0100

    Handling all signals.

--- lab-4/zad3/catcher.c
+++ lab-4/zad3/catcher.c
@@ -1,14 +1,45 @@
 #include <signal.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 
 int wait = 1;
+int show_time = 0;
+int count = 0;
+
+void print_cur_time() {
+	time_t now = time(0);
+	printf("%s", asctime(gmtime(&now)));
+}
 
 void handle(int sig_no, siginfo_t * info, void * ctx){
 	int sender = info->si_pid;
 	int instruction = info->si_value.sival_int;
-	printf("Otrzymano od procesu z PID: %d instrukcje nr %d\n", sender, instruction);
-	if (instruction == 5) wait = 0;
+	show_time = 0;
+	++count;
+	switch (instruction) {
+		case 1:
+			for (int i = 1; i <= 100; i++){
+				printf("%d\n", i);
+            }
+			break;
+		case 2:
+			print_cur_time();
+			break;
+		case 3:
+			printf("%d\n", count);
+			break;
+		case 4:
+			show_time = 1;
+			break;
+		case 5:
+			wait = 0;
+			break;
+		default:
+			break;
+	}
+
+
 	kill(sender, SIGUSR1);
 }
 
@@ -22,7 +53,8 @@ int main() {
 	printf("PID: %d\n", getpid());
 
 	while(wait){
-		pause();
+		if (show_time) print_cur_time();
+		sleep(1);
 	};
 	return 0;
 }
