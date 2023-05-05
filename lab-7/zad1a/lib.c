e016b551379fcdea850231035a38e2beafdd464d
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-05-05 10:33:07 +0200

    Added creation of semaphore

--- lab-7/zad1a/lib.c
+++ lab-7/zad1a/lib.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "lib.h"
 
-void test() {
-    printf("Hello, world!");
+Sema create_sema(char *name, int initial_value) {
+    key_t key = ftok(getenv("HOME"), name[0]);
+    if (key == -1) {
+        perror("ftok: ");
+        return 0;
+    }
+
+    Sema sema_id = semget(key, 1, 0664 | IPC_CREAT);
+    if (sema_id == -1) {
+        perror("semget: ");
+        return 0;
+    }
+
+    if (semctl(sema_id, 0, SETVAL, initial_value) == -1) {
+        perror("semctl: ");
+        return 0;
+    }
+
+    return sema_id;
+}
+
+Sema open_sema(char *name) {
+    key_t key = ftok(getenv("HOME"), name[0]);
+    if (key == -1) {
+        perror("ftok: ");
+        return 0;
+    }
+
+    Sema sema_id = semget(key, 1, 0);
+    if (sema_id == -1) {
+        perror("semget: ");
+        return 0;
+    }
+    return sema_id;
 }
--- lab-7/zad1a/lib.h
+++ lab-7/zad1a/lib.h
@@ -1,1 +1,14 @@
-void test();
+#define CLIENT_EXE "./client"
+#define BARBER_EXE "./barber"
+#define CLIENT_AMOUNT 10;
+#define BARBER_AMOUNT 6;
+#define CHAIR_AMOUNT 5;
+#define LOBBY_CAP 3;
+
+typedef int Sema;
+
+Sema create_sema(char * name, int initial_value);
+Sema open_sema(char * name);
+
+void close_sema(Sema sema_id);
+void destroy_sema(char * name);
