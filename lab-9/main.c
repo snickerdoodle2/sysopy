cfbeb8fa60aa97279dcd00d1a6302bbdb7a7d9fd
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-05-15 04:37:15 +0200

    Elf almost working

--- lab-9/main.c
+++ lab-9/main.c
@@ -1,24 +1,57 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 
+#define ELF_AMOUNT 10
+#define REINDEER_AMOUNT 9
+
 int waiting_elfs, reindeers_home;
-pthread_t reindeers[9];
-pthread_t elfs[10];
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
+
+pthread_t reindeers[REINDEER_AMOUNT];
+pthread_t elfs[ELF_AMOUNT];
 pthread_t santa;
 
 void * reindeer_handler() {
     printf("Reindeer\n");
+    while(1);
     return NULL;
 }
 
 void * elf_handler() {
-    printf("Elf\n");
+    unsigned int state;
+    state = time(NULL) ^ pthread_self();
+    while (1) {
+        int time_to_sleep = rand_r(&state) % 4 + 2;
+        sleep(time_to_sleep);
+        pthread_mutex_lock(&mutex);
+        if (waiting_elfs < 3) {
+            waiting_elfs++;
+            printf("Elf: czeka %d elfow na Mikolaja (%ld)\n", waiting_elfs, pthread_self());
+            fflush(stdout);
+            if (waiting_elfs == 3) {
+                // Wake up santa
+            }
+        }
+        pthread_mutex_unlock(&mutex);
+    }
     return NULL;
 }
 
 void * santa_handler() {
     printf("Santa\n");
+    sleep(10);
+    for (int i = 0; i < ELF_AMOUNT; i++) {
+        pthread_cancel(elfs[i]);
+    }
+
+
+    for (int i = 0; i < REINDEER_AMOUNT; i++) {
+        pthread_cancel(reindeers[i]);
+    }
     return NULL;
 }
 
@@ -26,11 +59,11 @@ int main() {
     waiting_elfs = 0;
     reindeers_home = 0;
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < REINDEER_AMOUNT; i++) {
         pthread_create(&reindeers[i], NULL, reindeer_handler, NULL);
     }
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < ELF_AMOUNT; i++) {
         pthread_create(&elfs[i], NULL, elf_handler, NULL);
     }
 
