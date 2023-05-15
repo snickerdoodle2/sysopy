911a3016fe6ba865c86f305f1884ff5312f6fe8a
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-05-15 06:51:29 +0200

    Es??

--- lab-9/main.c
+++ lab-9/main.c
@@ -30,6 +30,7 @@ void * reindeer_handler() {
     while (1) {
         int time_to_sleep = rand_r(&state) % 6 + 5;
         sleep(time_to_sleep);
+
         pthread_mutex_lock(&mutex);
         if (reindeers_home == 0) reindeers_wait = 1;
         reindeers_home++;
@@ -55,13 +56,16 @@ void * elf_handler() {
     unsigned int state;
     state = time(NULL) ^ pthread_self();
     while (1) {
-        int waiting = 0;
         int time_to_sleep = rand_r(&state) % 4 + 2;
         sleep(time_to_sleep);
+
         pthread_mutex_lock(&mutex);
-        if (waiting_elfs < 3) {
+        int locked_waiting = waiting_elfs;
+        pthread_mutex_unlock(&mutex);
+        
+        if (locked_waiting < 3) {
+            pthread_mutex_lock(&mutex);
             if (waiting_elfs == 0) elfs_wait = 1;
-            waiting = 1;
             waiting_elfs_tab[waiting_elfs] = pthread_self();
             waiting_elfs++;
             printf("Elf: czeka %d elfow na Mikolaja (%ld)\n", waiting_elfs, pthread_self());
@@ -71,14 +75,12 @@ void * elf_handler() {
                 fflush(stdout);
                 pthread_cond_broadcast(&condition);
             }
-        } else {
-            printf("Elf: samodzielnie rozwiazuje problem (%ld)\n", pthread_self());
-            fflush(stdout);
-        }
-        pthread_mutex_unlock(&mutex);
-        if (waiting) {
+            pthread_mutex_unlock(&mutex);
             pause();
             printf("Elf: Mikolaj rozwiazal moj problem! (%ld)\n", pthread_self());
+            fflush(stdout);
+        } else {
+            printf("Elf: samodzielnie rozwiazuje problem (%ld)\n", pthread_self());
             fflush(stdout);
         }
     }
@@ -89,6 +91,7 @@ void * santa_handler() {
     int gifts_amount = 0;
     unsigned int state;
     state = time(NULL) ^ pthread_self();
+
     while (gifts_amount < 3) {
         printf("Mikolaj: Zasypiam\n");
         fflush(stdout);
@@ -96,19 +99,24 @@ void * santa_handler() {
         while(reindeers_home < 9 && waiting_elfs < 3) {
             pthread_cond_wait(&condition, &mutex);
         }
+        pthread_mutex_unlock(&mutex);
         printf("Mikolaj: Budze sie\n");
         fflush(stdout);
 
         if (waiting_elfs == 3) {
             printf("Mikolaj: rozwiazuje problemy elfow: %ld, %ld, %ld\n", waiting_elfs_tab[0], waiting_elfs_tab[1], waiting_elfs_tab[2]);
             fflush(stdout);
+            pthread_mutex_lock(&mutex);
             elfs_wait = 0;
+            pthread_mutex_unlock(&mutex);
             for (int i = 0; i < 3; i++){
                 int repair_time = rand_r(&state) % 2 + 1;
                 sleep(repair_time);
                 pthread_kill(waiting_elfs_tab[i], SIGUSR1);
             }
+            pthread_mutex_lock(&mutex);
             waiting_elfs = 0;
+            pthread_mutex_unlock(&mutex);
         }
 
         if (reindeers_home == 9) {
@@ -117,11 +125,12 @@ void * santa_handler() {
             int gift_time = rand_r(&state) % 3 + 2;
             sleep(gift_time);
             gifts_amount++;
+            pthread_mutex_lock(&mutex);
             reindeers_wait = 0;
             reindeers_home = 0;
+            pthread_mutex_unlock(&mutex);
             pthread_cond_broadcast(&reindeers_cond);
         }
-        pthread_mutex_unlock(&mutex);
     }
     for (int i = 0; i < ELF_AMOUNT; i++) {
         pthread_cancel(elfs[i]);
