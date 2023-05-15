b6cc0990ccf30061be5d9d58d4fee871a23356c6
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-05-15 04:10:11 +0200

    Main thread waits for santa to finish

--- lab-9/main.c
+++ lab-9/main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 int waiting_elfs, reindeers_home;
+pthread_t reindeers[9];
+pthread_t elfs[10];
+pthread_t santa;
 
 void * reindeer_handler() {
+    printf("Reindeer\n");
     return NULL;
 }
 
 void * elf_handler() {
+    printf("Elf\n");
     return NULL;
 }
 
 void * santa_handler() {
+    printf("Santa\n");
     return NULL;
 }
 
 int main() {
     waiting_elfs = 0;
     reindeers_home = 0;
-
-    pthread_t reindeers[9];
-    pthread_t elfs[10];
-    pthread_t santa;
 
     for (int i = 0; i < 9; i++) {
         pthread_create(&reindeers[i], NULL, reindeer_handler, NULL);
@@ -32,7 +35,7 @@ int main() {
     }
 
     pthread_create(&santa, NULL, santa_handler, NULL);
-    
 
+    pthread_join(santa, NULL); // Wait until santa is finished
     return 0;
 }
